82b10953bf59c3a67703a63bec640965a566a2cc
DEBASMITROY2002 <ddruk2018@gmail.com>
2025-12-09 02:11:35 +0530

    backend fix chnaged to flask

--- target-backend/src/main.py
+++ target-backend/src/main.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
+from flask import Flask, request, jsonify
 from . import models, checksum
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = Flask(__name__)
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -15,29 +13,41 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/checksum-news/")
-def create_checksum_news(search_keys: list, db: Session = Depends(get_db)):
+@app.route("/checksum-news/", methods=['POST'])
+def create_checksum_news():
+    search_keys = request.get_json()
+    db_gen = get_db()
+    db = next(db_gen)
     result_checksum = checksum.checksum_news(search_keys)
     db_result = models.Result(endpoint="checksum-news", search_key=", ".join(search_keys), checksum=result_checksum)
     db.add(db_result)
     db.commit()
     db.refresh(db_result)
-    return db_result
+    next(db_gen, None)
+    return jsonify({"id": db_result.id, "endpoint": db_result.endpoint, "search_key": db_result.search_key, "checksum": db_result.checksum})
 
-@app.post("/checksum-images-news/")
-def create_checksum_images(search_keys: list, db: Session = Depends(get_db)):
+@app.route("/checksum-images-news/", methods=['POST'])
+def create_checksum_images():
+    search_keys = request.get_json()
+    db_gen = get_db()
+    db = next(db_gen)
     result_checksum = checksum.checksum_images(search_keys)
     db_result = models.Result(endpoint="checksum-images-news", search_key=", ".join(search_keys), checksum=result_checksum)
     db.add(db_result)
     db.commit()
     db.refresh(db_result)
-    return db_result
+    next(db_gen, None)
+    return jsonify({"id": db_result.id, "endpoint": db_result.endpoint, "search_key": db_result.search_key, "checksum": db_result.checksum})
 
-@app.post("/checksum-files/")
-def create_checksum_files(depth: int, db: Session = Depends(get_db)):
+@app.route("/checksum-files/", methods=['POST'])
+def create_checksum_files():
+    depth = int(request.args.get('depth'))
+    db_gen = get_db()
+    db = next(db_gen)
     result_checksum = checksum.checksum_files(depth)
     db_result = models.Result(endpoint="checksum-files", search_key=str(depth), checksum=result_checksum)
     db.add(db_result)
     db.commit()
     db.refresh(db_result)
-    return db_result
+    next(db_gen, None)
+    return jsonify({"id": db_result.id, "endpoint": db_result.endpoint, "search_key": db_result.search_key, "checksum": db_result.checksum})
